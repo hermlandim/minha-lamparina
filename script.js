5bdc720de90727a545dbc793892330971266fa45
hermerson.landim <hermerson_landim@hotmail.com>
2022-06-02 17:36:09 -0300

    Melhoria no JS

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ const buttons = document.getElementById('invisible')
 console.log(buttons);
 function lampBroken () {
     lamp.src = './img/quebrada.jpg'
-    button[0].setAttribute('disabled', 'disabled')
-    button[1].setAttribute('disabled', 'disabled')
+    button[0].setAttribute('hidden', 'hidden')
+    button[1].setAttribute('hidden', 'hidden')
     buttons.removeAttribute('hidden', '')
     console.log(buttons);
     buttons.addEventListener('click', concertar)
     function concertar() {
         lamp.src = './img/desligada.jpg'
-        button[0].removeAttribute('disabled')
-        button[1].removeAttribute('disabled')
+        button[0].removeAttribute('hidden')
+        button[1].removeAttribute('hidden')
         buttons.setAttribute('hidden', 'hidden')
     }
 }
